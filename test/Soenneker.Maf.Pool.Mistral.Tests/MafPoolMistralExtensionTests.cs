7accad5100c9d381e1935c3a28d4512aecf2e9e4
Jake Soenneker <jake@soenneker.com>
2026-04-21 19:36:18 -0500

    cleanup

--- test/Soenneker.Maf.Pool.Mistral.Tests/MafPoolMistralExtensionTests.cs
+++ test/Soenneker.Maf.Pool.Mistral.Tests/MafPoolMistralExtensionTests.cs
@@ -3,13 +3,8 @@ namespace Soenneker.Maf.Pool.Mistral.Tests;
 
 public sealed class MafPoolMistralExtensionTests
 {
-    public MafPoolMistralExtensionTests( output)
-    {
-    }
-
     [Test]
     public void Default()
     {
-
     }
 }
